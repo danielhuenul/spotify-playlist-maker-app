7b9fc6d1bbf908cbc4e65ae1dc6ffa20a3167f75
Daniel Huenul <danielhuenul8@gmail.com>
2022-10-23 16:08:23 -0300

    ADD: Implemented axios client

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,12 +1,19 @@
 import "./home.css"
 import wall01 from '../../../assets/wall01.jpg'
+import clientAdapter from "../../../service/client.adapter"
 
 const Home = () => {
+
+  const handleLogin = async () => {
+    const response = await clientAdapter.get("/login");
+    window.location.href = response.data.url
+  }
+
   return (
     <div className="main">
       <img src={wall01} alt="" className="background" />
       <h2>Playlist maker</h2>
-      <button id="login">Login in Spotify</button>
+      <button id="login" onClick={handleLogin}>Login in Spotify</button>
     </div>
   )
 }
