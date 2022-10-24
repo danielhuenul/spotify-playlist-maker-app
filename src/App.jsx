1795c5de7a6e73e91cf48cf9dfa8d3f75faa5349
Daniel Huenul <danielhuenul8@gmail.com>
2022-10-23 22:05:24 -0300

    React Router implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { Routes, Route, Link } from "react-router-dom";
+
 import "./app.css"
 import Home from "./components/pages/Home/Home";
+import Maker from "./components/pages/Maker/Maker";
+import About from "./components/pages/About/About";
 import Navbar from "./components/ui/NavBar/Navbar";
+import React from "react";
 
 export default function App() {
   return (
-    <div>
+    <React.Fragment>
       <Navbar />
-      <Home />
-    </div>
+      <Routes>
+        <Route path="/maker" element={<Maker />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </React.Fragment>
   )
 }
---
+++ src/components/pages/Maker/Maker.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Maker = () => {
+  return (
+    <div>Maker</div>
+  )
+}
+
+export default Maker
