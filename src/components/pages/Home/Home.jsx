560328812ac3ebe5063c7d4a2ed3b6ebee569476
Daniel Huenul <danielhuenul8@gmail.com>
2022-11-05 18:49:32 -0300

    ADD: added styles for maker component

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,20 +1,32 @@
-import "./home.css"
-import wall01 from '../../../assets/wall01.jpg'
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { Context } from "../../../Context";
 import clientAdapter from "../../../service/adapters/client.adapter"
+import wall01 from '../../../assets/wall01.jpg'
+import "./home.css"
 
 const Home = () => {
+  const navigation = useNavigate();
+  const [ context ] = useContext(Context);
 
   const handleLogin = async () => {
-    const response = await clientAdapter.get("/login")
-    localStorage.setItem("simpleToken", response.data.simpleToken)
-    window.location.href = response.data.url
+    if (!context?.auth?.id) {
+      const response = await clientAdapter.get("/login")
+      localStorage.setItem("simpleToken", response.data.simpleToken)
+      window.location.href = response.data.url
+      return
+    }
+
+    navigation("/maker");
   }
 
   return (
     <div className="main">
-      <img src={wall01} alt="" className="background" />
+      <img src={wall01} alt="dj wallpaper" className="background" />
       <h2>Playlist maker</h2>
-      <button id="login" onClick={handleLogin}>Login in Spotify</button>
+      <button id="login" onClick={handleLogin}>
+        { context?.auth?.id === "" ? "Login in Spotify" : "Go to Maker"}
+      </button>
     </div>
   )
 }
