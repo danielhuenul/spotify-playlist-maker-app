5f219f08c32ec4ca12e33138a06d265c2044e431
Daniel Huenul <danielhuenul8@gmail.com>
2022-11-06 13:33:31 -0300

    ADD: added docker config and nginx config file

--- src/service/adapters/client.adapter.js
+++ src/service/adapters/client.adapter.js
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
-const clientAdapter = axios.create({
-  baseURL: "http://localhost:3200"
+export default axios.create({
+  baseURL: "http://34.170.164.245"
 })
-
-export default clientAdapter
